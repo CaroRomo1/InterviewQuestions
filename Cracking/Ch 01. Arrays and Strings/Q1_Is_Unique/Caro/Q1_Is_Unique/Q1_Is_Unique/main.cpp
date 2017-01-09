939ecea8ff7d70fd54b57271b84b6b362fe7a7c7
CaroRomo1 <cr_p1@hotmail.com>
2017-01-09 11:53:54 -0600

    Chapter 1 Question 1 Solution Caro

---
+++ Cracking/Ch 01. Arrays and Strings/Q1_Is_Unique/Caro/Q1_Is_Unique/Q1_Is_Unique/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Q1_Is_Unique
+//
+//  Created by Carolina Romo on 1/9/17.
+//  Copyright © 2017 ITESM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
