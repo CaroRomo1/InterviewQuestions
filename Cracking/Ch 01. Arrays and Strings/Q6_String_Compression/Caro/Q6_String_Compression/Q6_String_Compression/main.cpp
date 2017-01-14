39d0b258de5989cb42a678e716d536e3f7067922
CaroRomo1 <cr_p1@hotmail.com>
2017-01-14 15:39:19 -0600

    Chapter 1 Question 6 Solution Caro

--- Cracking/Ch 01. Arrays and Strings/Q6_String_Compression/Caro/Q6_String_Compression/Q6_String_Compression/main.cpp
+++ Cracking/Ch 01. Arrays and Strings/Q6_String_Compression/Caro/Q6_String_Compression/Q6_String_Compression/main.cpp
@@ -8,12 +8,61 @@
 /*
  * Cracking the coding interview edition 6
  * Chapter 1 Question 6
+ * String Compression: 
+ * Implement a method to perform basic string compression using the counts of
+ * repeated characters. For example, the string aabcccccaaa would become a2blc5a3.
+ * If the "compressed"string would not become smaller than the original string, 
+ * your method should return the original string. You can assume the string 
+ * has only uppercase and lowercase letters (a - z).
  */
 
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-int main() {
+string compress(string str) {
+    int originalLength = str.length();
+    // We assume that if it has a lenght smaller than 2, it could not be compressed
+    if (originalLength < 2) {
+        return str;
+    }
+    string out = "";
+    int count = 1;
+    for(int i = 1; i < originalLength; i++) {
+        if (str[i-1] == str[i]) {
+            count++;
+        }
+        else {
+            out += str[i-1];
+            out += to_string(count);
+            count = 1;
+        }
+        if (out.length() >= originalLength) {
+            return str;
+        }
+    }
     
+    out += str[originalLength-1];
+    out += to_string(count);
+    
+    if (out.length() >= originalLength) {
+        return str;
+    }
+    return out;
+}
+
+int main() {
+    string str, compressedStr;
+    cout << "Enter a string:" << endl;
+    cin >> str;
+
+    compressedStr = compress(str);
+    if (str.compare(compressedStr)) {
+        cout << str << " can be compressed to " << compressedStr << endl;
+    } else {
+        cout << str << " can not be compressed" << endl;
+    }
     return 0;
 }
+
