16758dec7a279941e299c190b8751f7bdd151f05
CaroRomo1 <cr_p1@hotmail.com>
2017-02-13 10:33:11 -0600

    Top Tec 2145

---
+++ TopTecProgrammer/2145 - Beauty Contest/2145 - Beauty Contest/2145 - Beauty Contest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2145 - Beauty Contest
+//
+//  Created by Carolina Romo on 2/13/17.
+//  Copyright © 2017 CaroRomo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
